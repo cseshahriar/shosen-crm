d1a3eda4226e4b5cf69e530fcca0ed506c50543b
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-11-24 00:10:56 +0600

    login.vue template created

--- crm_vue/src/router/index.js
+++ crm_vue/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import SignUp from '../views/SignUp.vue'
+import LogIn from '../views/LogIn.vue'
 import Navbar from '@/components/layout/Navbar'
 
 const routes = [
@@ -13,6 +14,11 @@ const routes = [
     path: '/sign-up',
     name: 'SignUp',
     component: SignUp
+  },
+  {
+    path: '/log-in',
+    name: 'LogIn',
+    component: LogIn
   },
   {
     path: '/about',
