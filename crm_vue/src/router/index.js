8aea973f5a5f38028368df67ee247a23600a4522
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-11-24 00:05:50 +0600

    created signup template

--- crm_vue/src/router/index.js
+++ crm_vue/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import SignUp from '../views/SignUp.vue'
 import Navbar from '@/components/layout/Navbar'
 
 const routes = [
@@ -7,6 +8,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
   },
   {
     path: '/about',
