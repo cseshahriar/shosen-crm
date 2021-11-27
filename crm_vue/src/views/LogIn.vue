21324949d605d561c21e14d84b017c6c3c504374
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-11-27 23:36:15 +0600

    login with vue

--- crm_vue/src/views/LogIn.vue
+++ crm_vue/src/views/LogIn.vue
@@ -3,19 +3,23 @@
         <div class="columns">
             <div class="column is-4 is-offset-4">
                 <h1 class="title">Log In</h1>
-                <form>
+                <form @submit.prevent="submitForm">
                     <div class="field">
                         <label>Email</label>
                         <div class="control">
-                            <input type="email" name="email" class="input">
+                            <input type="text" name="email" v-model="username" class="input">
                         </div>
                     </div>
 
                     <div class="field">
                         <label>Password</label>
                         <div class="control">
-                            <input type="password" name="password" class="input">
+                            <input type="password" name="password" v-model="password" class="input">
                         </div>
+                    </div>
+
+                    <div class="notification is-warning" v-if="errors.length">
+                        <p v-for="error in errors" :key="error">{{ error }}</p>
                     </div>
 
                     <div class="field">
@@ -30,10 +34,65 @@
 </template>
 
 <script>
+    import axios from 'axios'
+    import {toast} from 'bulma-toast'
+
     export default {
         name: 'LogIn',
+        data() {
+            return {
+                username: '',
+                password: '',
+                errors: []
+            }
+        },
+        methods: {
+            submitForm() {
+                axios.defaults.headers.common['Authorization'] = '' 
+                localStorage.removeItem('token')
+
+                this.errors = []
+
+                // validation
+                if(this.username === '') {
+                    this.errors.push('The username is missing')
+                }
+
+                if(this.password === '') {
+                    this.errors.push('The password is missing')
+                }
+
+                if(!this.errors.length) {
+                    const formData = {
+                        username: this.username,
+                        password: this.password,
+                    }
+
+                    axios
+                    .post('/api/v1/token/login/', formData)
+                    .then(response => {
+                        const token = response.data.auth_token
+                        this.$store.commit('setToken', token)
+                        axios.defaults.headers.common['Authorization'] = 'Token ' + token
+                        localStorage.setItem('token', token)
+                        this.$router.push('/dashboard/my-account')
+                    })
+                    .catch(error => {
+                        if(error.response) {
+                            for (const property in error.response.data) {
+                                this.errors.push(`${property}: ${error.response.data[property]}`)
+                            }
+                        } else if(error.message ) {
+                            this.errors.push('Something went wrong. Please try again!')
+                        }
+                    })
+
+                }
+            }
+        }
     }
 </script>
+
 
 <style scoped>
 
