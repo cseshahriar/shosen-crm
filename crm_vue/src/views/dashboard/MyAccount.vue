4396358543e911499efd18f7fb30bcfbf3fa9416
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-11-27 23:49:58 +0600

    vue logout

--- crm_vue/src/views/dashboard/MyAccount.vue
+++ crm_vue/src/views/dashboard/MyAccount.vue
@@ -4,13 +4,40 @@
             <div class="column is-12">
               <h1 class="title">My Account</h1>
             </div>
+
+            <div class="column is-12">
+                <button @click="logout()" class="button is-danger">Log out</button>
+            </div>
+
         </div>
     </div>    
 </template>
 
 <script>
+    import axios from 'axios'
+    import {toast} from 'bulma-toast'
+
     export default {
         name: 'MyAccount',
+        methods: {
+            async logout() {
+                await axios
+                    .post('/api/v1/token/logout')
+                    .then(response => {
+                        console.log('logout')
+                    })
+                    .catch(
+                        error => {
+                            console.log(JSON.stringify(error));
+                        }
+                    )
+
+                    axios.defaults.headers.common['Authorization'] = ''     
+                    localStorage.removeItem('token')
+                    this.$store.commit('removeToken')
+                    this.$router.push('/log-in')
+            }
+        }
     }
 </script>
 
