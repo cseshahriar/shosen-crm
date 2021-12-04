9c067864330c19a9d718d892d2373f662cc7ed98
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-12-04 22:34:31 +0600

    UserDetail api view

--- crm_django/team/views.py
+++ crm_django/team/views.py
@@ -30,3 +30,27 @@ class TeamViewSet(viewsets.ModelViewSet):
         obj.members.add(self.request.user) # m2m save 
         obj.save()
         
+        
+class UserDetail(APIView):
+    """ user detail and update """
+    def get_object(self, pk):
+        """ get object """
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    
