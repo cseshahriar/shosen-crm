4b3c92259891828eeede59568e008294e7c39f25
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-12-05 22:45:16 +0600

    noteviewset api created

--- crm_django/client/views.py
+++ crm_django/client/views.py
@@ -34,3 +34,20 @@ class ClientViewSet(viewsets.ViewSet):
         return self.queryset.filter(team=team)
     
     
+class NoteViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    queryset = Note.objects.all()
+    
+    def get_queryset(self):
+        team = Team.objects.filter(members__in=[self.request.user]).first()
+        client_id = self.request.GET.get('client_id')
+        return self.queryset.filter(team=team).filter(client_id=client_id).first()
+    
+    def perform_create(self, serializer):
+        team = Team.objects.filter(members__in=[self.request.user]).first()
+        client_id = self.request.data['client_id']
+        serializer.save(team=team, created_by=self.request.user, client_id=client_id)
+    
+    
+    
+    
