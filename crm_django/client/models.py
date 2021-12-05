dc5c39d451a7966100e97f1db7b6faafb3e368a5
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-12-05 09:30:46 +0600

    Note model created

--- crm_django/client/models.py
+++ crm_django/client/models.py
@@ -19,3 +19,22 @@ class Client(models.Model):
     
     def __str__(self):
         return self.name
+    
+    
+class Note(models.Model):
+    team = models.ForeignKey(
+        Team, related_name='notes', on_delete=models.CASCADE
+    )
+    client = models.ForeignKey(
+        Client, related_name='notes', on_delete=models.CASCADE
+    )
+    created_by = models.ForeignKey(
+        User, related_name='notes', on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+    body = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
